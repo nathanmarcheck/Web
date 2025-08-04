4b89e7202472153223591268dd5e534afdfdabb8
nathanmarcheckneves <nathanmarcheckneves@gmail.com>
2025-08-04 17:40:44 -0300

    ajuste

--- pratica/js/ex15.js
+++ pratica/js/ex15.js
@@ -2,10 +2,10 @@ var quantlitros, preco, desconto, result;
 quantlitros = parseFloat(prompt("Digite a quantidade de litros: "));
 preco = parsefloat(5.9)
 if (quantlitros >= 10 && quantlitros <= 20) {
-    desconto = quantlitros * preco * 0.1; 
-    alert("Valor com desconto: R$ " + (quantlitros * preco - desconto).toFixed(2));
+    desconto = (quantlitros * preco) * 0.9; 
+    alert("Valor com desconto: R$ " + desconto.toFixed(2));
 }
 else{
-    desconto = quantlitros * preco * 0.2; 
-    alert("Valor com desconto: R$ " + (quantlitros * preco - desconto).toFixed(2));
+    desconto = (quantlitros * preco )* 0.8; 
+    alert("Valor com desconto: R$ " + desconto.toFixed(2));
 }
